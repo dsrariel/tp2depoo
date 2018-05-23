128bb9cb787d764694b39251ae8c9871409a8e08
JhonnyTA <38565598+JhonnyTA@users.noreply.github.com>
2018-05-23 19:26:19 -0300

    Banco.h sem as referências e parâmetro de excluirCliente como string

--- Banco.h
+++ Banco.h
@@ -1,5 +1,5 @@
-#ifndef BANCO_H 
-#define BANCO_H
+#ifndef BANCO_H_INCLUDED
+#define BANCO_H_INCLUDED
 
 #include <string>
 #include <list>
@@ -22,23 +22,23 @@ class Banco{
     void setDados();
 
     //Getters
-    const ListaDeClientes& getClientes();
-    const ListaDeContas& getContas();
+    const ListaDeClientes getClientes();
+    const ListaDeContas getContas();
     void getDados();
-    const int& getNovoNumeroConta(); //Gera novo número de conta para criação de conta 
+    const int getNovoNumeroConta(); //Gera novo número de conta para criação de conta
 
     //Outros
-    const int& inserirCliente(const Cliente& cliente); //Retorna 1 caso cliente não exista (sucesso) e 0 caso já exista (falha)
-    const int& excluirCliente(const int& cpfCnpj); //Retorna 1 caso cliente não possua conta (sucesso) e 0 caso possua (falha)
+    const int inserirCliente(const Cliente& cliente); //Retorna 1 caso cliente não exista (sucesso) e 0 caso já exista (falha)
+    const int excluirCliente(const std::string& cpfCnpj); //Retorna 1 caso cliente não possua conta (sucesso) e 0 caso possua (falha)
     void inserirConta(const Cliente& cliente);
-    const int& excluirConta (const int& numeroConta); //Retorna 1 caso conta exista (sucesso) e 0 caso não exista (falha)
-    const int& depositar (const int& numeroConta, const double& valor); //Retorna 1 caso conta exista (sucesso) e 0 caso não exista (falha)
-    const int& sacar (const int& numeroConta, const double& valor); //Retorna 1 caso conta exista (sucesso) e 0 caso não (falha)
-    const int& transferencia (const int& numeroContaOrigem, const int& numeroContaDestino, const double& valor); //Retorna 1 caso contas existam (sucesso) e 0 caso não (falha)
+    const int excluirConta (const int& numeroConta); //Retorna 1 caso conta exista (sucesso) e 0 caso não exista (falha)
+    const int depositar (const int& numeroConta, const double& valor); //Retorna 1 caso conta exista (sucesso) e 0 caso não exista (falha)
+    const int sacar (const int& numeroConta, const double& valor); //Retorna 1 caso conta exista (sucesso) e 0 caso não (falha)
+    const int transferencia (const int& numeroContaOrigem, const int& numeroContaDestino, const double& valor); //Retorna 1 caso contas existam (sucesso) e 0 caso não (falha)
     void tarifa();
     void cpmf();
-    const int& saldoConta(const int& numeroConta); //Retorna o quê caso conta não exista?
-    const ListaDeMovimentacoes& extratoMes(const int& numeroConta); //Retorna o quê caso a conta não exista?
-    const ListaDeMovimentacoes& extratoDatas(const time_t& inicio, const time_t& fim); //Retorna o quê caso a conta não exista?
+    const int saldoConta(const int& numeroConta); //Retorna o quê caso conta não exista?
+    const ListaDeMovimentacoes extratoMes(const int& numeroConta); //Retorna o quê caso a conta não exista?
+    const ListaDeMovimentacoes extratoDatas(const time_t& inicio, const time_t& fim); //Retorna o quê caso a conta não exista?
 };
-#endif
+#endif // BANCO_H_INCLUDE
