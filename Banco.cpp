cd40cfee9abbb102d35af46a16699745e943f046
Ariel Ribeiro <dsrariel@gmail.com>
2018-05-23 20:43:31 -0300

    Ajustes das atribuições e das funções

--- Banco.cpp
+++ Banco.cpp
@@ -4,41 +4,44 @@ Banco::Banco(const std::string& nomeBanco){
     this->nomeBanco = nomeBanco;
 }
 
-const ListaDeClientes  Banco::getClientes(){        //retorna a lista de clientes do banco
+const ListaDeClientes  Banco::getClientes(){
     return this->clientes;
 }
 
-const ListaDeContas Banco::getContas(){             //retorna a lista de contas do banco
+const ListaDeContas Banco::getContas(){        
     return this->contas;
 }
 
-const int Banco::inserirCliente(const Cliente& cliente){
-    auto it = std::find(this->clientes.begin(),this->clientes.end(),cliente);
-    if(it == this->clientes.end()){     //cliente não existe no banco
+bool Banco::inserirCliente(const Cliente& cliente){
+    auto it = std::find(this->clientes.begin(), this->clientes.end(), cliente);
+    if(it == this->clientes.end()){
         this->clientes.push_back(cliente);
-        return 1;                       //sucesso
-    }else{
-        return 0;                       //cliente ja existe no banco, falha
+        return 1;
+    }
+    else{
+        return 0;
     }
 }
 
-const int Banco::excluirCliente(const std::string& cpfCnpj){
-    for (std::list<Cliente>::iterator it1 = this->clientes.begin(); it1 != this->clientes.end(); it1++){    //procura cliente na lista de clientes
-        if(strcmp(it1.cpfCnpj,cpfCnpj)==0){          //cliente encontrado na lista
-            for (std::list<Conta>::iterator it2 = this->contas.begin(); it2 != this->contas.end(); it2++){ //verifica se o cliente possui uma conta no banco
-                if(it2.cliente==it1){    //cliente possui conta
-                    std::cout<<"Cliente possui conta, falha ao exclui-lo. "<<'\n';
-                    return 0;           //falha
+bool Banco::excluirCliente(const std::string& cpfCnpj){
+    ListaDeClientes::iterator it1;
+    ListaDeContas::iterator it2;
+    for (it1 = this->clientes.begin(); it1 != this->clientes.end(); it1++){
+        //Cliente encontrado
+        if(it1->getCpfCnpj == cpfCnpj){
+            for (it2 = this->contas.begin(); it2 != this->contas.end(); it2++){    
+                //Conta encontrada
+                if(it2->getCliente==it1){
+                    return 0;
                 }
             }
-             //Cliente nao possui conta
-             this->clientes.erase(it1);     //exclui o cliente
-             std::cout<<"O cliente foi excluido com sucesso."<<'\n';
-             return 1;                      //sucesso
+            //Conta não encontrada
+            this->clientes.erase(it1);    
+            return 1;                
         }
     }
-     std::cout<<"O cliente nao existe no banco."<<'\n';
-     return 0;        //falha
+    //Cliente não encotrado
+    return 0;
 }
 
 void Banco::inserirConta(const Cliente& cliente){
--- Banco.h
+++ Banco.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <list>
+#include <algorithm>
 #include "Cliente.h"
 #include "Conta.h"
 
@@ -28,17 +29,17 @@ class Banco{
     const int getNovoNumeroConta(); //Gera novo número de conta para criação de conta
 
     //Outros
-    const int inserirCliente(const Cliente& cliente); //Retorna 1 caso cliente não exista (sucesso) e 0 caso já exista (falha)
-    const int excluirCliente(const std::string& cpfCnpj); //Retorna 1 caso cliente não possua conta (sucesso) e 0 caso possua (falha)
+    bool inserirCliente(const Cliente& cliente); //Retorna True caso cliente não exista (sucesso) e False caso já exista (falha)
+    bool excluirCliente(const std::string& cpfCnpj); //Retorna True caso cliente não possua conta (sucesso) e False caso possua (falha)
     void inserirConta(const Cliente& cliente);
-    const int excluirConta (const int& numeroConta); //Retorna 1 caso conta exista (sucesso) e 0 caso não exista (falha)
-    const int depositar (const int& numeroConta, const double& valor); //Retorna 1 caso conta exista (sucesso) e 0 caso não exista (falha)
-    const int sacar (const int& numeroConta, const double& valor); //Retorna 1 caso conta exista (sucesso) e 0 caso não (falha)
-    const int transferencia (const int& numeroContaOrigem, const int& numeroContaDestino, const double& valor); //Retorna 1 caso contas existam (sucesso) e 0 caso não (falha)
+    bool excluirConta (int numeroConta); //Retorna True caso conta exista (sucesso) e False caso não exista (falha)
+    bool depositar (int numeroConta, double valor); //Retorna True caso conta exista (sucesso) e False caso não exista (falha)
+    bool sacar (int numeroConta, double valor); //Retorna True caso conta exista (sucesso) e False caso não (falha)
+    bool transferencia (int numeroContaOrigem, int numeroContaDestino, double valor); //Retorna True caso contas existam (sucesso) e False caso não (falha)
     void tarifa();
     void cpmf();
-    const int saldoConta(const int& numeroConta); //Retorna o quê caso conta não exista?
-    const ListaDeMovimentacoes extratoMes(const int& numeroConta); //Retorna o quê caso a conta não exista?
-    const ListaDeMovimentacoes extratoDatas(const time_t& inicio, const time_t& fim); //Retorna o quê caso a conta não exista?
+    int saldoConta(int numeroConta); //Retorna o quê caso conta não exista?
+    const ListaDeMovimentacoes extratoMes(int numeroConta); //Retorna o quê caso a conta não exista?
+    const ListaDeMovimentacoes extratoDatas(tm inicio, tm fim); //Retorna o quê caso a conta não exista?
 };
 #endif // BANCO_H_INCLUDE
