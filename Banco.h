8d1cbbcd637100990d738a2300462718f257bd99
JhonnyTA <38565598+JhonnyTA@users.noreply.github.com>
2018-05-24 18:11:14 -0500

    descricao de tarifa e saldo conta

--- Banco.h
+++ Banco.h
@@ -36,9 +36,9 @@ class Banco{
     bool depositar (int numeroConta, double valor); //Retorna True caso conta exista (sucesso) e False caso não exista (falha)
     bool sacar (int numeroConta, double valor); //Retorna True caso conta exista (sucesso) e False caso não (falha)
     bool transferencia (int numeroContaOrigem, int numeroContaDestino, double valor); //Retorna True caso contas existam (sucesso) e False caso não (falha)
-    void tarifa();
+    void tarifa();//debita, caso tenha saldo suficiente, 15 reais de cada conta
     void cpmf();
-    int saldoConta(int numeroConta); //Retorna o quê caso conta não exista?
+    int saldoConta(int numeroConta); ////Retorna -1 caso conta não exista
     const ListaDeMovimentacoes extratoMes(int numeroConta); //Retorna o quê caso a conta não exista?
     const ListaDeMovimentacoes extratoDatas(tm inicio, tm fim); //Retorna o quê caso a conta não exista?
 };
