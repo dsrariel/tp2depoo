c0a369ce3e4d0964203c5c77d5e396d317f1bcb2
JhonnyTA <38565598+JhonnyTA@users.noreply.github.com>
2018-05-28 20:22:22 -0500

    ajustes de retornos

--- Banco.h
+++ Banco.h
@@ -33,17 +33,18 @@ class Banco{
 
     //Outros
     bool inserirCliente(const Cliente& cliente); //Retorna True caso cliente não exista (sucesso) e False caso já exista (falha)
-    bool excluirCliente(const std::string& cpfCnpj); //Retorna True caso cliente não possua conta (sucesso) e False caso possua (falha)
+    int excluirCliente(const std::string& cpfCnpj); //Retorna 1 caso cliente não possua conta (sucesso), 0 caso possua uma conta e -1 caso não esteja cadastrado(falha)
     void inserirConta(const Cliente& cliente);
     bool excluirConta (int numeroConta); //Retorna True caso conta exista (sucesso) e False caso não exista (falha)
     bool depositar (int numeroConta, double valor); //Retorna True caso conta exista (sucesso) e False caso não exista (falha)
-    bool sacar (int numeroConta, double valor); //Retorna True caso conta exista (sucesso) e False caso não (falha)
+    int sacar (int numeroConta, double valor); //Retorna 1 caso conta exista e tenha saldo suficiente(sucesso),0 caso não tenha o saldo e -1 caso conta não exista (falha)
     bool transferencia (int numeroContaOrigem, int numeroContaDestino, double valor); //Retorna True caso contas existam (sucesso) e False caso não (falha)
     void tarifa();//debita, caso tenha saldo suficiente, 15 reais de cada conta
     void cpmf();
-    double saldoConta(int numeroConta); ////Retorna -1 caso conta não exista
+    double saldoConta(int numeroConta); //Retorna -1 caso conta não exista
     const ListaDeMovimentacoes extratoMes(int numeroConta); //Retorna o quê caso a conta não exista?
     const ListaDeMovimentacoes extratoDataInicial(int numeroConta, tm inicio); //Retorna o quê caso a conta não exista?
-    const ListaDeMovimentacoes extratoDatas(int numeroConta , tm inicio, tm fim); //Retorna o quê caso a conta não exista?
+    const ListaDeMovimentacoes extratoDatas(int numeroConta, tm inicio, tm fim); //Retorna o quê caso a conta não exista?
 };
+
 #endif // BANCO_H_INCLUDE
